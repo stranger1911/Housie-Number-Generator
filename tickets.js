a91618edc5c78cc3e023edccea741f3988d8a2b5
stranger1911 <khansufiyanahmed2@gmail.com>
2024-04-27 16:37:13 +0530

    commit

--- tickets.js
+++ tickets.js
@@ -28,7 +28,7 @@ let row3 = [18, 19, 20, 21, 22, 23, 24, 25, 26];
 
 let randomNumber;
 
-if (window.location.pathname === "/Housie-Tickets/") {
+if (window.location.pathname === "/ticket.html") {
   ticketBox.forEach((e) => {
     e.innerHTML = "";
   });
